02f29e52164a9a9aa80b96bf08f0eba0942d3928
kruffka <kruffko@gmail.com>
2023-09-10 19:55:10 +0700

    Hello types

---
+++ 1_hello_types/format.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 12345;
+    printf("a %7d a\n", a);
+    printf("b %-7d b\n", a);
+
+}
---
+++ 1_hello_types/types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 5;
+    char ch = 'A';
+    float f = 5.132;
+    printf("%d %c\n", a, ch);
+    printf("%f\n", f);
+
+}
