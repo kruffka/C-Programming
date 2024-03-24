ad166747eca1bc9d96b630d94cefb2cdd7f274f6
a.veyler <a.veyler@1440.space>
2024-03-24 16:19:29 +0300

    Update 13 make1 clean. Add double link list example

--- 14_linkedLists/list_funcs.c
+++ 14_linkedLists/list_funcs.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 typedef struct node node_t;
 
 // Односвязный список
 struct node {
-    int val;
-    node_t *next;
+    int val; // value - некоторое целочисленное значение
+    node_t *next; // указатель на структуру node_t (будет указывать на следующий элемент либо равна NULL)
 };
 
+// Выделяет память под новый элемент списка, зануляет указатель на следующий и присваивает значение val из аргумента val
 node_t *new_node(int val) {
 
     node_t *new_node = (node_t *)malloc(sizeof(node_t));
@@ -18,15 +19,48 @@ node_t *new_node(int val) {
     return new_node;
 }
 
-void insert(node_t *prev, node_t *next) {
+// Вставляем элемент в конец списка
+void push_back(node_t **list, int val) {
+    node_t *tmp = *list;
+    if (tmp == NULL) {
+        *list = new_node(val);
+        return;
+    }
+    while (tmp->next) {
+        tmp = tmp->next;
+    }
+    tmp->next = new_node(val);
+}
+
+// Удаляем элемент вначале списка
+void pop_front(node_t **list) {
+
+    node_t *head = *list;
+    node_t *new_list = head->next;
+    free(head);
+    *list = new_list;
+}
+
+
+// Вставляем новый элемент посреди списка
+void insert(node_t *prev, int val) {
+    if (prev == NULL) {
+        printf("prev node is NULL!\n");
+        return;
+    }
+ 
+    // Выделяем память под новый элемент
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    // Заполняем новый элемент
+    new_node->val = val;
+    // Вставляем новый элемент между двумя
+    new_node->next = prev->next;
+    prev->next = new_node;
 
 }
 
-void pop() {
 
-}
-
-// recursive
+// Рекурсивное освобождение списка
 void free_list(node_t *node) {
 
     if (node) {
@@ -36,6 +70,7 @@ void free_list(node_t *node) {
 
 }
 
+// Не рекурсивный вывод на экран списка
 void print_list(node_t *node) {
     
     while (node) {
@@ -45,20 +80,57 @@ void print_list(node_t *node) {
     printf("NULL\n");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
+    if (argc < 2) {
+        printf("Usage: ./a.out N\n");
+        return -1;
+    }
+    int N = atoi(argv[1]);
 
-    node_t *list_p, *my_list = NULL;
-    list_p = new_node(0);
-    my_list = list_p;
-    list_p->next = new_node(1);
-    list_p = list_p->next;
-    list_p->next = new_node(2);
-    list_p = list_p->next;
-    list_p->next = new_node(3);
+    node_t *prev_node = NULL, *my_list = NULL;
+
+    // Создаем список из N элементов
+    for (int i = 0; i < N; i++) {
+        node_t *cur_node = NULL;
+        if (prev_node == NULL) {
+            cur_node = new_node(i);
+            prev_node = cur_node;
+            my_list = cur_node;
+        } else {
+            cur_node = new_node(i);
+            prev_node->next = cur_node;
+        }
+        prev_node = cur_node;
+    }
+
+    // Выводим на экран наш список
+    printf("My list:\n");
+    print_list(my_list);
+
+    // push_back
+    printf("List after push back:\n");
+    push_back(&my_list, 123);
+    push_back(&my_list, 42);
 
     print_list(my_list);
 
+    printf("List after pop front:\n");
+    pop_front(&my_list);
 
+    print_list(my_list);
+
+    printf("List after insert after third elem:\n");
+    node_t *tmp = my_list;
+    for (int i = 0; i < 3; i++) {
+        tmp = tmp->next;
+    }
+    insert(tmp, 555);
+    print_list(my_list);
+
+    // Удаление списка
     free_list(my_list);
+    my_list = NULL;
+
+
 }
