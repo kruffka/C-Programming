2aae1ad86f2836b159d771bfca3127a54053836e
kruffka <kruffko@gmail.com>
2026-09-08 11:26:22 +0300

    fix hello.c

--- 0_hello_world/src/apples.c
+++ 0_hello_world/src/apples.c
@@ -6,5 +6,5 @@ int main()
 
     printf("I have %d apples\n", apples);
 
-    return 0;
+    return 1;
 }
--- 0_hello_world/src/hello.c
+++ 0_hello_world/src/hello.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-int main()
-{
-    print("hello, world!\n");
-
+int main() {
+    printf("hello, world!\n");
     return 0;
 }
